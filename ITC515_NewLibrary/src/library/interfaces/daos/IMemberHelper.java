78e682564caa36743ae97043b5c2b32da7ef1c8e
srikanth reddy sama <srikanthsama@192-168-1-6.tpgi.com.au>
2016-09-21 18:46:01 +1000

    created objects

--- ITC515_NewLibrary/src/library/interfaces/daos/IMemberHelper.java
+++ ITC515_NewLibrary/src/library/interfaces/daos/IMemberHelper.java
@@ -5,5 +5,8 @@ import library.interfaces.entities.IMember;
 public interface IMemberHelper {
 	
 	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id);
+{
+	return new Mmber(firstName, lastName, contactPhone, emailAddress, id);
+}
 
 }
