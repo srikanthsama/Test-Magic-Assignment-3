998f8b0efdf1e6dd578c3363bf8d84af3da11574
srikanth reddy sama <srikanthsama@srikanths-MacBook-Pro.local>
2016-09-27 21:03:11 +1000

    Made changes to memberMapDAO.java class

--- ITC515_NewLibrary/src/library/MemberMapDAO.java
+++ ITC515_NewLibrary/src/library/MemberMapDAO.java
@@ -11,9 +11,6 @@ import library.interfaces.daos.IMemberHelper;
 
 
 public class MemberMapDAO implements IMemberDAO{
-	private IMemberHelper helper;
-	private Map<Integer, IMember> memberMap;
-	private int nextID;
 	
 	public MemberMapDAO(IMemberHelper helper) {
 		if (helper == null ) {
@@ -35,78 +32,4 @@ public class MemberMapDAO implements IMemberDAO{
 	}
 
 	
-	@Override
-	public IMember addMember(String firstName, String lastName,
-			String contactPhone, String emailAddress) {
-		int id = getNextId();
-		IMember mem = helper.makeMember(firstName, lastName, contactPhone, emailAddress, id);
-		memberMap.put(Integer.valueOf(id), mem);
-		return mem;
-	}
-
-	@Override
-	public IMember getMemberByID(int id) {
-		if (memberMap.keySet().contains(Integer.valueOf(id))) {
-			return memberMap.get(Integer.valueOf(id));
-		}
-		return null;
-	}
-
-	@Override
-	public List<IMember> listMembers() {
-		List<IMember> list = new ArrayList<IMember>(memberMap.values());
-		return Collections.unmodifiableList(list);
-	}
-
-	@Override
-	public List<IMember> findMembersByLastName(String lastName) {
-		if ( lastName == null || lastName.isEmpty()) {
-			throw new IllegalArgumentException(
-				String.format("MemberMapDAO : findMembersByLastName : lastName cannot be null or blank"));
-		}
-		List<IMember> list = new ArrayList<IMember>();
-		for (IMember m : memberMap.values()) {
-			if (lastName.equals(m.getLastName())) {
-				list.add(m);
-			}
-		}
-		return Collections.unmodifiableList(list);
-	}
-
-	@Override
-	public List<IMember> findMembersByEmailAddress(String emailAddress) {
-		if ( emailAddress == null || emailAddress.isEmpty()) {
-			throw new IllegalArgumentException(
-				String.format("MemberMapDAO : findMembersByEmailAddress : emailAddress cannot be null or blank"));
-		}
-		List<IMember> list = new ArrayList<IMember>();
-		for (IMember m : memberMap.values()) {
-			if (emailAddress.equals(m.getEmailAddress())) {
-				list.add(m);
-			}
-		}
-		return Collections.unmodifiableList(list);
-	}
-
-	@Override
-	public List<IMember> findMembersByNames(String firstName, String lastName) {
-		if ( firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
-			throw new IllegalArgumentException(
-				String.format("MemberMapDAO : findMembersByNames : firstName and lastName cannot be null or blank"));
-		}
-		List<IMember> list = new ArrayList<IMember>();
-		for (IMember m : memberMap.values()) {
-			if (firstName.equals(m.getFirstName()) && lastName.equals(m.getLastName())) {
-				list.add(m);
-			}
-		}
-		return Collections.unmodifiableList(list);
-	}
-
-	private int getNextId() {
-		return nextID++;
-	}
-
-
-
-}
+	
