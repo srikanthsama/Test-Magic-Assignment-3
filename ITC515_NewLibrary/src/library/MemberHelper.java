7a2a644bfb641108d7723cc715570bbc7aa5037b
srikanth reddy sama <srikanthsama@srikanths-MacBook-Pro.local>
2016-09-27 20:53:30 +1000

    made changes in memberHelper.java class

--- ITC515_NewLibrary/src/library/MemberHelper.java
+++ ITC515_NewLibrary/src/library/MemberHelper.java
@@ -3,10 +3,12 @@ import library.entities.Member;
 import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
 
-public class MemberHelper implements IMemberHelper {
-	@Override
+public class MemberHelper implements IMemberHelper
+ {
+
 	public IMember makeMember(String firstName, String lastName,
-			String contactPhone, String emailAddress, int id) {
+			String contactPhone, String emailAddress, int id)
+ {
 		
 		return new Member(firstName, lastName, contactPhone, emailAddress, id);
 	}
