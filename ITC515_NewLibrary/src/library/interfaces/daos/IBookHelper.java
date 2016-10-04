a46189623651bb683a88983fee8ce9665e4ffe8b
11580932 <hari786raj@gmail.com>
2016-10-04 22:44:34 +1100

    entities changes

--- ITC515_NewLibrary/src/library/interfaces/daos/IBookHelper.java
+++ ITC515_NewLibrary/src/library/interfaces/daos/IBookHelper.java
@@ -3,7 +3,9 @@ package library.interfaces.daos;
 import library.interfaces.entities.IBook;
 
 public interface IBookHelper {
-	
+	@Override
+	public IBook makeBook(String author, String title, String callNumber, int id) {
+		return new Book(author, title, callNumber, id);
 	public IBook makeBook(String author, String title, String callNumber, int id);
 
 }
